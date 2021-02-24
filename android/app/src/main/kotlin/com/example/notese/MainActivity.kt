1bf06ee84d7b8ba3a3aa023d9fbc1e4a64e38fb2
SUMIT KUMAR <52107131+sumitvarun@users.noreply.github.com>
2021-02-25 04:04:38 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/notese/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.notese
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/notescreen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class NotesScreen extends StatefulWidget {
+  NotesScreen({Key key}) : super(key: key);
+
+  @override
+  _NotesScreenState createState() => _NotesScreenState();
+}
+
+class _NotesScreenState extends State<NotesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          SizedBox(height: 40.0,),
+          Padding(padding: EdgeInsets.symmetric(horizontal: 30.0),
+          child: Row(mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Container(height: 50.0, width: 50.0,)
+          ],),),
+        ],
+      ),
+    )
+  }
+}
