777868db3ee347ab8c5237b65d78e255e210efe9
SUMIT KUMAR <52107131+sumitvarun@users.noreply.github.com>
2021-02-25 14:34:32 +0530

    update

--- lib/screens/notescreen.dart
+++ lib/screens/notescreen.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class NotesScreen extends StatefulWidget {
@@ -10,6 +11,44 @@ class NotesScreen extends StatefulWidget {
 }
 
 class _NotesScreenState extends State<NotesScreen> {
+  int _selectedCategoryIndex = 0;
+
+  Widget _buildCategoryCard(int index, String title, String content) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
+      height: 240.0,
+      width: 175.0,
+      decoration: BoxDecoration(
+        color: _selectedCategoryIndex == index
+            ? Color(0xFF417BFB)
+            : Color(0xFF417BFB),
+        borderRadius: BorderRadius.circular(20.0),
+        boxShadow: [
+          _selectedCategoryIndex == index
+              ? BoxShadow(
+                  color: Colors.black26, offset: Offset(0, 2), blurRadius: 10.0)
+              : BoxShadow(color: Colors.transparent)
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Text(
+              title,
+              style: TextStyle(
+                  color: _selectedCategoryIndex == index
+                      ? Colors.white
+                      : Color(0xffafb4c6)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +67,7 @@ class _NotesScreenState extends State<NotesScreen> {
                   width: 50.0,
                   decoration: BoxDecoration(
                     image: DecorationImage(
-                      image: AssetImage('images\Avatar.png'),
+                      image: AssetImage('images/Avatar.png'),
                     ),
                   ),
                 ),
@@ -43,6 +82,29 @@ class _NotesScreenState extends State<NotesScreen> {
                   ),
                 ),
               ],
+            ),
+          ),
+          SizedBox(
+            height: 40.0,
+          ),
+          Container(
+            height: 200.0,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: categories.length + 1,
+              itemBuilder: (BuildContext context, int index) {
+                if (index == 0) {
+                  return SizedBox(
+                    width: 20.0,
+                  );
+                }
+                var categories;
+                return _buildCategoryCard(
+                  index - 1,
+                  categories.keys.toList()[index - 1],
+                  categories.value.toList()[index - 1],
+                );
+              },
             ),
           ),
         ],
